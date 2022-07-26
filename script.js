14e91d64aef1637172588e169a2e9e5c04607807
SnowFox-95 <321mts95@gmail.com>
2022-07-26 11:20:07 +0300

    final

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const dom = {
     new_task: document.getElementById('new_task'),
     add: document.getElementById('add'),
-    tasks: document.getElementById('tasks')
+    tasks: document.getElementById('tasks'),
+    count: document.getElementById('count')
 }
 
 //Массив задач
@@ -72,18 +73,28 @@ function tasksRender(list) {
     })
 
     dom.tasks.innerHTML = htmlList
+
+    renderTaskCount(list)
 }
 
 //Отслеживаем клик по чекбоксу задачи
 dom.tasks.onclick = (event) => {
     const target = event.target
-    const isCheckboxClickEl = target.classList.contains('todo_checkbox-div')
+    const isCheckboxEl = target.classList.contains('todo_checkbox-div')
+    const isDeleteEl = target.classList.contains('todo_task-del')
 
-    if (isCheckboxClickEl) {
+    if (isCheckboxEl) {
         const task = target.parentElement.parentElement
         const taskId = task.getAttribute('id')
         changeTaskStatus(taskId, tasks)
         tasksRender(tasks)
+    }
+    if (isDeleteEl) {
+        const task = target.parentElement
+        const taskId = task.getAttribute('id')
+        deleteTask(taskId, tasks)
+        tasksRender(tasks)
+        
     }
 }
 
@@ -94,4 +105,18 @@ function changeTaskStatus(id, list) {
             task.isComplete = !task.isComplete
         }
     })
+}
+
+//Функция удаления задачи
+function deleteTask(id, list) {
+    list.forEach((task, idx) => {
+        if (task.id == id) {
+            list.splice(idx, 1)
+        }
+    })
+}
+
+// Вывод количества задач
+function renderTaskCount(list) {
+    dom.count.innerHTML = list.length
 }
